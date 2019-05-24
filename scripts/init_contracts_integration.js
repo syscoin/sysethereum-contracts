2a6e4ebbcf8f48c2b12a990cb3a66b130f645ccc
sidhujag <sidhujag@gmail.com>
2019-05-24 16:36:36 -0700

    init work

--- scripts/init_contracts_integration.js
+++ scripts/init_contracts_integration.js
@@ -5,7 +5,7 @@ module.exports = async function(callback) {
   var sb = await Superblocks.deployed();
 
   var blocksMerkleRoot = "0x00000448444b0b3ddf48863024870d158bf48ce7f02a1d3093b01a84c1c1da42";
-  var accumulatedWork = web3.toBigNumber("0");
+  var accumulatedWork = web3.toBigNumber(0x100001);
   var timestamp = 1552606661;
   var prevTimestamp = 0;
   var lastHash = "0x00000448444b0b3ddf48863024870d158bf48ce7f02a1d3093b01a84c1c1da42";
