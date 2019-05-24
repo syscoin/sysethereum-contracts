502bf22d847a96d9d83e8bbddbf796274f435575
sidhujag <sidhujag@gmail.com>
2019-05-24 16:16:28 -0700

    update bits

--- scripts/init_contracts_integration.js
+++ scripts/init_contracts_integration.js
@@ -4,12 +4,12 @@ module.exports = async function(callback) {
   
   var sb = await Superblocks.deployed();
 
-  var blocksMerkleRoot = "0x000007e0aeb37995d0c0ae8ad49fb7ae17eba65167501f85d40d736d67e754c3";
+  var blocksMerkleRoot = "0x00000448444b0b3ddf48863024870d158bf48ce7f02a1d3093b01a84c1c1da42";
   var accumulatedWork = web3.toBigNumber("0");
   var timestamp = 1552606661;
   var prevTimestamp = 0;
-  var lastHash = "0x000007e0aeb37995d0c0ae8ad49fb7ae17eba65167501f85d40d736d67e754c3";
-  var lastBits = 0x1e0ffff0;
+  var lastHash = "0x00000448444b0b3ddf48863024870d158bf48ce7f02a1d3093b01a84c1c1da42";
+  var lastBits = 0x1e0fffff;
   var parentId = "0x0";
   var height = 0;
   await sb.initialize(blocksMerkleRoot, accumulatedWork, timestamp, prevTimestamp, lastHash, lastBits, parentId,height);
