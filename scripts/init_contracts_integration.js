6d5bf7024d0a477159059d94a5e028d0fae87cdf
sidhujag <sidhujag@gmail.com>
2019-07-01 22:34:23 -0700

    update genesis

--- scripts/init_contracts_integration.js
+++ scripts/init_contracts_integration.js
@@ -5,7 +5,7 @@ module.exports = async function(callback) {
   
   var sb = await SyscoinSuperblocks.deployed();
   console.log("beginning deployment...");
-  var blocksMerkleRoot = "0x0000022642db0346b6e01c2a397471f4f12e65d4f4251ec96c1f85367a61a7ab";
+  var blocksMerkleRoot = "0xa395b884dea0c77d3eff856838ebba4e74a6e633c7516784a7f21705d3d58f45";
   var accumulatedWork = web3.utils.toBN("0x0000000000000000000000000000000000000000000b5aea51981d092e7d9739");
   var timestamp = 1562016306;
   var lastHash = "0xa395b884dea0c77d3eff856838ebba4e74a6e633c7516784a7f21705d3d58f45"; // 46800
