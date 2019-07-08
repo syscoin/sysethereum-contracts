269754dd0a62153dca5ec1602463c2998519f826
sidhujag <sidhujag@gmail.com>
2019-07-08 16:14:40 -0700

    update testnet genesis

--- scripts/init_contracts_integration.js
+++ scripts/init_contracts_integration.js
@@ -5,12 +5,12 @@ module.exports = async function(callback) {
   
   var sb = await SyscoinSuperblocks.deployed();
   console.log("beginning deployment...");
-  var blocksMerkleRoot = "0xb106dd7496392e50b7c7e529240d6e62987db827ec2a7203120b0ca3c5a83c71";
-  var accumulatedWork = web3.utils.toBN("0x0000000000000000000000000000000000000000000b5ad3ace4939af72d8700");
+  var blocksMerkleRoot = "0x00000c8ce2dbffb1958260498de24e52c083dd0eaf2eec95baadff55b6d160da";
+  var accumulatedWork = web3.utils.toBN("0x0000000000000000000000000000000000000000000000000000000016800168");
   var timestamp = 1562016284;
-  var lastHash = "0xb106dd7496392e50b7c7e529240d6e62987db827ec2a7203120b0ca3c5a83c71"; // 46799
+  var lastHash = "0x00000c8ce2dbffb1958260498de24e52c083dd0eaf2eec95baadff55b6d160da"; // 359
   var parentId = "0x0";
-  var lastBits = 403358244;
+  var lastBits = 504365055;
   console.log("Initializing...");
   res = await sb.initialize(blocksMerkleRoot, accumulatedWork, timestamp, lastHash, lastBits, parentId);
   console.log("init_contracts_integration end");
