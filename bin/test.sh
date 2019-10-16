45b997bea6339df1d633dd09d004aa6dceaf3c1d
KBRiverun <no-reply@your-mama.com>
2019-10-16 15:44:59 +0200

    test.sh: Initialize Openzeppelin

--- bin/test.sh
+++ bin/test.sh
@@ -5,6 +5,7 @@ set -e
 npx ganache-cli --gasLimit 47000000000  -e 1000000 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
+npx oz init sysethereum-contracts 1.0.0
 npx truffle compile
 npx truffle migrate --reset --network development
 npx truffle test
