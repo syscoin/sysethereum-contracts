5328bbbb5e47b181d3db7a26a8a9e81136d8cbfe
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-18 12:34:04 -0800

    update package

--- org/sysethereum/agents/contract/SyscoinSuperblocks.java
+++ org/sysethereum/agents/contract/SyscoinSuperblocks.java
@@ -127,6 +127,7 @@ public class SyscoinSuperblocks extends Contract {
 
     static {
         _addresses = new HashMap<String, String>();
+        _addresses.put("4", "0xA63DC15Dc407be3aa50664d85a7e5E563bFb5e10");
     }
 
     @Deprecated
