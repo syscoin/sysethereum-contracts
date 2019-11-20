b469ca60e6f588aeda56ed8764dd8bb6b06acedc
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-19 16:05:01 -0800

    update claimmgr interface to include checkClaimFinished used by timeout in battlemgr

--- contracts/interfaces/SyscoinClaimManagerI.sol
+++ contracts/interfaces/SyscoinClaimManagerI.sol
@@ -4,8 +4,7 @@ import './SyscoinSuperblocksI.sol';
 
 interface SyscoinClaimManagerI {
     function bondDeposit(bytes32 superblockHash, address account, uint amount) external returns (uint);
-
     function getDeposit(address account) external view returns (uint);
-
+    function checkClaimFinished(bytes32 superblockHash) external returns (bool);
     function sessionDecided(bytes32 superblockHash, address winner, address loser) external;
 }
