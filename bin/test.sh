402febb5559a51752f5dfb8f1382e47345f67440
sidhujag <jsidhu@blockchainfoundry.co>
2019-11-28 11:32:48 -0800

    update test script for CI

--- bin/test.sh
+++ bin/test.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-npx ganache-cli --gasLimit 47000000000  -e 1000000 2> /dev/null 1> /dev/null &
+ganache-cli --gasLimit 4000000000000 -e 1000000 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
-npx oz init sysethereum-contracts 1.0.0
-npx truffle compile
-npx truffle migrate --reset --network development
-npx truffle test
+rm -rf .openzeppelin
+oz init sysethereum-contracts 1.0.0
+truffle compile
+truffle migrate --reset --network development
+truffle test
 
 kill -9 $(lsof -t -i:8545)
