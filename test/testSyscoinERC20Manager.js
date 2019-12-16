af8e6cd2292370cfbbb825e3904fb37ec777b2c9
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-12-15 22:20:33 -0800

    tests for new cancel transfer logic

--- test/testSyscoinERC20Manager.js
+++ test/testSyscoinERC20Manager.js
@@ -18,10 +18,14 @@ const SyscoinERC20ManagerV1 = Contracts.getFromLocal('SyscoinERC20ManagerForTest
 var SyscoinERC20 = artifacts.require("./token/SyscoinERC20.sol");
 
 const { expectRevert } = require('openzeppelin-test-helpers');
+const { blockchainTimeoutSeconds } = require('./utils');
 
 
 contract('SyscoinERC20Manager', function(accounts) {
   const owner = accounts[1];
+  const cancelAddress = accounts[2];
+  const randomAddress = accounts[3];
+  const challangerAddress = accounts[4];
   const proxyAdmin = accounts[9];
   const value =   2000000000;
   const burnVal = 1000000000;
@@ -48,10 +52,11 @@ contract('SyscoinERC20Manager', function(accounts) {
     assert.equal(await erc20Manager.methods.trustedRelayerContract().call(), trustedRelayerContract, "trustedRelayerContract is not correct");
     assert.equal(await erc20Asset.balanceOf(owner), value, `ERC20Asset's ${owner} balance is not the expected one`);
 
-    await erc20Manager.methods.freezeBurnERC20(burnVal, assetGUID, erc20Asset.address, 8, syscoinAddress).send({from: owner, gas: 300000});
+    let tx = await erc20Manager.methods.freezeBurnERC20(burnVal, assetGUID, erc20Asset.address, 8, syscoinAddress).send({from: owner, gas: 300000});
     assert.equal(await erc20Asset.balanceOf(erc20Manager.options.address), burnVal, "erc20Manager balance is not correct");
     assert.equal(await erc20Asset.balanceOf(owner), value - burnVal, `erc20Asset's user balance after burn is not the expected one`);
     assert.equal(await erc20Manager.methods.assetBalances(assetGUID).call(), burnVal, `assetBalances for ${assetGUID} GUID is not correct`);
+    assert.equal(1, tx.events.TokenFreeze.returnValues.bridgetransferid, 'bridgetransferid should be 1');
   });
 
 
@@ -135,36 +140,168 @@ contract('SyscoinERC20Manager', function(accounts) {
   });
 
   describe("Bridge transfer cancellation", () => {
+    let wrongBridgeTransferId = 299;
+    let erc20ManagerForCancel, erc20AssetCancel;
+    let bridgetransferid;
+    const CANCEL_MINT_TIMEOUT = 907200; // 1.5 weeks in seconds
+    const CANCEL_TRANSFER_TIMEOUT = 3600; // 1 hour in seconds
+
+    beforeEach("Prepare env", async () => {
+      erc20ManagerForCancel = await this.project.createProxy(SyscoinERC20ManagerV0, {
+        initMethod: 'init',
+        initArgs: [utils.SYSCOIN_REGTEST, trustedRelayerContract]
+      });
+
+      erc20AssetCancel = await SyscoinERC20.new("SyscoinToken", "SYSX", 8, {from: owner});
+      await erc20AssetCancel.assign(cancelAddress, value);
+      await erc20AssetCancel.approve(erc20ManagerForCancel.options.address, burnVal, {from: cancelAddress});
+
+      let tx = await erc20ManagerForCancel.methods.freezeBurnERC20(burnVal, assetGUID, erc20AssetCancel.address, 8, syscoinAddress).send({from: cancelAddress, gas: 300000});
+      bridgetransferid = tx.events.TokenFreeze.returnValues.bridgetransferid;
+    });
+
     describe("cancelTransferRequest()", () => {
       describe("should fail when", () => {
-        it("bridgeTransferId is incorrect");
-        it("BridgeTransferStatus is not Ok");
-        it("msg.sender is not bridgeTransfer.tokenFreezerAddress");
-        it("msg.sender is not bridgeTransfer.tokenFreezerAddress");
-        it("Freeze is not old enough");
-        it("Cancel deposit is not enough");
+        it("bridgeTransferId is incorrect", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferRequest(wrongBridgeTransferId).send({from: cancelAddress}),
+            "#SyscoinERC20Manager cancelTransferRequest(): Status of bridge transfer must be Ok"
+          );
+        });
+
+        it("BridgeTransferStatus is not Ok", async () => {
+          let bt = await erc20ManagerForCancel.methods.getBridgeTransfer(wrongBridgeTransferId).call();
+          assert.notEqual('1', bt['_status'], "Status must not be Ok (1) for this test case");
+
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferRequest(0).send({from: cancelAddress}),
+            "#SyscoinERC20Manager cancelTransferRequest(): Status of bridge transfer must be Ok"
+          );
+        });
+
+        it("msg.sender is not bridgeTransfer.tokenFreezerAddress", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferRequest(bridgetransferid).send({from: randomAddress}),
+            "#SyscoinERC20Manager cancelTransferRequest(): Only msg.sender is allowed to cancel"
+          );
+        });
+
+        it("Freeze is not old enough", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferRequest(bridgetransferid).send({from: cancelAddress}),
+            "#SyscoinERC20Manager cancelTransferRequest(): Transfer must be at least 1.5 week old"
+          );
+        });
+
+        it("Cancel deposit is not enough", async () => {
+          // travel in time 1.5 weeks forward
+          await blockchainTimeoutSeconds(CANCEL_MINT_TIMEOUT);
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferRequest(bridgetransferid).send({from: cancelAddress}),
+            "#SyscoinERC20Manager cancelTransferRequest(): Cancel deposit incorrect"
+          );
+        });
       })
 
-      it("should process cancel request succesfully");
+      it("should process cancel request succesfully", async () => {
+        // travel in time 1.5 weeks forward
+        await blockchainTimeoutSeconds(CANCEL_MINT_TIMEOUT);
+        let tx = await erc20ManagerForCancel.methods.cancelTransferRequest(bridgetransferid).send({from: cancelAddress, value: web3.utils.toWei('3', 'ether')});
+        assert.equal(cancelAddress, tx.events.CancelTransferRequest.returnValues.canceller, "msg.sender incorrect");
+        assert.equal(1, tx.events.CancelTransferRequest.returnValues.bridgetransferid, "bridgetransferid incorrect");
+      });
     })
 
     describe("cancelTransferSuccess()", () => {
       describe("should fail when", () => {
-        it("bridgeTransferId is incorrect");
-        it("BridgeTransferStatus is not CancelRequested");
+        it("bridgeTransferId is incorrect", async() => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferSuccess(wrongBridgeTransferId).send({from: cancelAddress}),
+            "#SyscoinERC20Manager cancelTransferSuccess(): Status must be CancelRequested"
+          );
+        });
+
+        it("BridgeTransferStatus is not CancelRequested", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.cancelTransferSuccess(bridgetransferid).send({from: cancelAddress}),
+            "#SyscoinERC20Manager cancelTransferSuccess(): Status must be CancelRequested"
+          );
+        });
       })
 
-      it("should process cancel transfer request succesfully");
+      it("should process cancel transfer request succesfully", async () => {
+        let startingErc20Bal = await erc20AssetCancel.balanceOf(cancelAddress);
+        let startingAssetGUIDBal = await erc20ManagerForCancel.methods.assetBalances(assetGUID).call();
+
+        // travel in time 1.5 weeks forward
+        await blockchainTimeoutSeconds(CANCEL_MINT_TIMEOUT);
+        await erc20ManagerForCancel.methods.cancelTransferRequest(bridgetransferid).send({from: cancelAddress, value: web3.utils.toWei('3', 'ether')});
+
+        let startingEthBal = parseInt(await web3.eth.getBalance(cancelAddress));
+
+        // travel in time 1 hour forward
+        await blockchainTimeoutSeconds(CANCEL_TRANSFER_TIMEOUT+1);
+        let tx = await erc20ManagerForCancel.methods.cancelTransferSuccess(bridgetransferid).send({from: cancelAddress});
+
+        let finalEthBal = parseInt(await await web3.eth.getBalance(cancelAddress));
+        let finalErc20Bal = await erc20AssetCancel.balanceOf(cancelAddress);
+        let finalAssetGUIDBal = await erc20ManagerForCancel.methods.assetBalances(assetGUID).call();
+
+        // we are doing 2.99 ETH because some ether got used for TX fees
+        assert(startingEthBal + parseInt(web3.utils.toWei('2.99', 'ether')) < finalEthBal, "Ether balance incorrect");
+        assert.equal(startingErc20Bal.toNumber() + burnVal, finalErc20Bal.toNumber(), "ERC20 balance incorrect");
+        assert.equal(startingAssetGUIDBal - burnVal, finalAssetGUIDBal, "Asset GUID balance incorrect");
+
+        assert.equal(tx.events.CancelTransferSucceeded.returnValues.canceller, cancelAddress, "canceller incorrect")
+        assert.equal(tx.events.CancelTransferSucceeded.returnValues.bridgetransferid, bridgetransferid, "bridgetransferid incorrect")
+
+        let bt = await erc20ManagerForCancel.methods.getBridgeTransfer(bridgetransferid).call();
+        assert.equal(bt._status, '4', "Bridge Transfer status incorrect");
+      });
     })
 
     describe("processCancelTransferFail()", () => {
       describe("should fail when", () => {
-        it("caller is not onlyTrustedRelayer");
-        it("bridgeTransferId is incorrect");
-        it("BridgeTransferStatus is not CancelRequested");
+        it("caller is not onlyTrustedRelayer", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.processCancelTransferFail(bridgetransferid, challangerAddress).send({from: randomAddress}),
+            "Call must be from trusted relayer"
+          );
+        });
+
+        it("bridgeTransferId is incorrect", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.processCancelTransferFail(wrongBridgeTransferId, challangerAddress).send({from: trustedRelayerContract}),
+            "#SyscoinERC20Manager cancelTransferSuccess(): Status must be CancelRequested to Fail the transfer"
+          );
+        });
+
+        it("BridgeTransferStatus is not CancelRequested", async () => {
+          await expectRevert(
+            erc20ManagerForCancel.methods.processCancelTransferFail(bridgetransferid, challangerAddress).send({from: trustedRelayerContract}),
+            "#SyscoinERC20Manager cancelTransferSuccess(): Status must be CancelRequested to Fail the transfer"
+          );
+        });
       })
 
-      it("should processCancelTransferFail succesfully");
+      it("should processCancelTransferFail succesfully", async () => {
+        // travel in time 1.5 weeks forward
+        await blockchainTimeoutSeconds(CANCEL_MINT_TIMEOUT);
+        await erc20ManagerForCancel.methods.cancelTransferRequest(bridgetransferid).send({from: cancelAddress, value: web3.utils.toWei('3', 'ether')});
+
+        let startingEthBal = parseInt(await web3.eth.getBalance(challangerAddress));
+
+        let tx = await erc20ManagerForCancel.methods.processCancelTransferFail(bridgetransferid, challangerAddress).send({from: trustedRelayerContract});
+
+        let finalEthBal = parseInt(await web3.eth.getBalance(challangerAddress));
+        assert.equal(startingEthBal + parseInt(web3.utils.toWei('3', 'ether')), finalEthBal, "Ether balance incorrect");
+
+        assert.equal(tx.events.CancelTransferFailed.returnValues.canceller, cancelAddress, "canceller incorrect")
+        assert.equal(tx.events.CancelTransferFailed.returnValues.bridgetransferid, bridgetransferid, "bridgetransferid incorrect")
+
+        let bt = await erc20ManagerForCancel.methods.getBridgeTransfer(bridgetransferid).call();
+        assert.equal(bt._status, '3', "Bridge Transfer status incorrect");
+      });
     })
   })
 });
