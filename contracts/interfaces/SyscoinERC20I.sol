92cadd8ce1d1ed22886b4ccd55026fcaaf139f15
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-03 10:57:39 -0700

    remove safeerc from interface

--- contracts/interfaces/SyscoinERC20I.sol
+++ contracts/interfaces/SyscoinERC20I.sol
@@ -4,5 +4,4 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 contract SyscoinERC20I is IERC20 {
     function decimals() external view returns (uint8);
-    using SafeERC20 for IERC20;
 }
