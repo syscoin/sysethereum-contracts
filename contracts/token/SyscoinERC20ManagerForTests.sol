76ea99676c6cd6b101d608d56f262e8802a9b082
sidhujag <jsidhu@blockchainfoundry.co>
2020-06-03 10:56:42 -0700

    use safe transfer from SyscoinERC20I

--- contracts/token/SyscoinERC20ManagerForTests.sol
+++ contracts/token/SyscoinERC20ManagerForTests.sol
@@ -4,6 +4,7 @@ import "./SyscoinERC20Manager.sol";
 
 // DONOT USE IN PRODUCTION
 contract SyscoinERC20ManagerForTests is SyscoinERC20Manager {
+    using SafeERC20 for SyscoinERC20I;
     // keyhash or scripthash for syscoinWitnessProgram
     function freezeBurnERC20(
         uint value,
@@ -26,7 +27,7 @@ contract SyscoinERC20ManagerForTests is SyscoinERC20Manager {
 
         SyscoinERC20I erc20 = SyscoinERC20I(erc20ContractAddress);
         require(precision == erc20.decimals(), "Decimals were not provided with the correct value");
-        erc20.transferFrom(msg.sender, address(this), value);
+        erc20.safeTransferFrom(msg.sender, address(this), value);
         emit TokenFreeze(msg.sender, value, 0);
 
         return true;
