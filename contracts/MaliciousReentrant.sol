a195275acdbfde8ef3cb20e6b40776e629131f27
jagdeep sidhu <sidhujag@syscoin.org>
2025-03-05 22:54:22 -0800

    updatae reentrant test

--- contracts/MaliciousReentrant.sol
+++ contracts/MaliciousReentrant.sol
@@ -13,63 +13,59 @@ contract MaliciousReentrant is IERC1155Receiver {
     SyscoinVaultManager public vault;
     address public erc1155Asset;
     bool public didAttack;
+    bool public attackReverted; // to track if the vault call reverted
 
     constructor(address _vault, address _erc1155Asset) {
         vault = SyscoinVaultManager(_vault);
         erc1155Asset = _erc1155Asset;
         didAttack = false;
+        attackReverted = false;
     }
 
-    // We call this to deposit tokens from the Malicious contract to the vault
-    // This will trigger onERC1155Received.
     function doAttack(uint tokenId, uint amount) external {
-        // we first need to have the tokens in this contract
-        // then we call safeTransferFrom => which triggers onERC1155Received
+        // to test re-entrancy, we will call safeTransferFrom => triggers onERC1155Received
         IERC1155(erc1155Asset).safeTransferFrom(msg.sender, address(this), tokenId, amount, "");
     }
 
     // =============== IERC1155Receiver Implementation ================
     function onERC1155Received(
-        address operator,
-        address from,
+        address /*operator*/,
+        address /*from*/,
         uint256 id,
         uint256 value,
-        bytes calldata data
+        bytes calldata /*data*/
     ) external override returns (bytes4) {
-        // If this is the first time, attempt re-entrancy:
+        // If this is the first time, attempt re-entrancy
         if (!didAttack) {
             didAttack = true;
-            // Attempt to re-enter the vault by calling freezeBurn
-            // We pass 'value', 'erc1155Asset' etc.
-            // The malicious logic => we want to see if it reverts due to nonReentrant
             try vault.freezeBurn(
                 value,
                 erc1155Asset,
                 id,
                 "sysMaliciousAddress"
             ) {
-                // If it doesn't revert, re-entrancy was successful (which is BAD)
-                // We'll store a flag or revert
+                // If it didn't revert, that means re-entrancy was successful
                 revert("Malicious attack succeeded, re-entrancy is possible!");
             } catch {
-                // The expected path is that it reverts due to `nonReentrant`.
+                // expected revert due to nonReentrant
+                attackReverted = true;
             }
         }
         return this.onERC1155Received.selector;
     }
 
     function onERC1155BatchReceived(
-        address operator,
-        address from,
-        uint256[] calldata ids,
-        uint256[] calldata values,
-        bytes calldata data
-    ) external override returns (bytes4) {
+        address /*operator*/,
+        address /*from*/,
+        uint256[] calldata /*ids*/,
+        uint256[] calldata /*values*/,
+        bytes calldata /*data*/
+    ) external pure override returns (bytes4) {
         return this.onERC1155BatchReceived.selector;
     }
 
     function supportsInterface(bytes4 interfaceId) external pure override returns (bool) {
-        return interfaceId == this.onERC1155Received.selector
+        return interfaceId == this.onERC1155Received.selector 
             || interfaceId == this.onERC1155BatchReceived.selector;
     }
 }
--- test/testMaliciousReentrant.js
+++ test/testMaliciousReentrant.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
-const truffleAssert = require('truffle-assertions');
+const truffleAssert = require("truffle-assertions");
+
 const SyscoinVaultManager = artifacts.require("SyscoinVaultManager");
 const MockERC1155 = artifacts.require("MockERC1155");
 const MaliciousReentrant = artifacts.require("MaliciousReentrant");
@@ -7,32 +8,47 @@ const MaliciousReentrant = artifacts.require("MaliciousReentrant");
 contract("Malicious Re-Entrancy Test", (accounts) => {
   let vault, mock1155, attacker;
   const owner = accounts[1]; // user that owns tokens
-  const trustedRelayer = accounts[0]; // vault constructor param
+  const trustedRelayer = accounts[0]; // for the vault's constructor
 
   before(async () => {
-    vault = await SyscoinVaultManager.new(trustedRelayer, 8888, false);
+    vault = await SyscoinVaultManager.new(
+      trustedRelayer, 
+      8888,    // SYSAssetGuid 
+      false    // testNetwork
+    );
+
     mock1155 = await MockERC1155.new("https://test.api/");
-    // Deploy attacker contract
     attacker = await MaliciousReentrant.new(vault.address, mock1155.address);
 
-    // Mint some tokens to the 'owner'
+    // Mint some ERC1155 tokens (id=777, amount=10) to the 'owner'
     await mock1155.mint(owner, 777, 10);
 
-    // Approve the attacker to move them => so we can do safeTransferFrom
-    await mock1155.setApprovalForAll(attacker.address, true, {from: owner});
+    // Owner must approve the 'attacker' contract to take them
+    await mock1155.setApprovalForAll(attacker.address, true, { from: owner });
   });
 
   it("Attempt re-entrancy => should revert", async () => {
-    // The attacker calls doAttack => triggers safeTransferFrom from 'owner'
-    // => The token goes to 'attacker' => onERC1155Received is called
-    // => attacker calls vault.freezeBurn => expected to fail (nonReentrant).
+    // Initially, didAttack = false, attackReverted = false
+    let didAttackInitial = await attacker.didAttack();
+    let attackRevertedInitial = await attacker.attackReverted();
+    expect(didAttackInitial).to.equal(false);
+    expect(attackRevertedInitial).to.equal(false);
+
+    // The attacker calls doAttack => triggers safeTransferFrom 
+    // => onERC1155Received => calls vault.freezeBurn => revert
     await truffleAssert.reverts(
-      attacker.doAttack(777, 1, {from: owner}),
-      "Malicious attack succeeded" // The revert reason from the contract if it DID succeed
+      attacker.doAttack(777, 1, { from: owner }),
+      "Malicious attack succeeded" // If re-entrancy was possible, we'd get this revert reason
     );
-    // If it reverts with "Returned error: VM Exception while processing..."
-    // that means our malicious attempt was blocked by nonReentrant or 
-    // because we forcibly revert in the malicious contract.
-    // We can also check the `didAttack` flag or other logs as needed.
+
+    // Now check the flags
+    let didAttackFinal = await attacker.didAttack();
+    let attackRevertedFinal = await attacker.attackReverted();
+
+    // We expect didAttackFinal == true => meaning the code path was triggered
+    expect(didAttackFinal).to.equal(true, "didAttack should be true after doAttack attempt");
+
+    // We expect attackRevertedFinal == true => meaning the vault's freezeBurn call failed
+    expect(attackRevertedFinal).to.equal(true, "attackReverted should be true => revert from nonReentrant");
   });
 });
